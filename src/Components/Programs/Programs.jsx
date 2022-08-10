295d216dc2297d30a57e1ad2bce9843c4c09b32e
uday kumar <udaykumar@inr.group>
2022-08-10 11:38:37 +0530

    plan Card

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Programs.css"
+import RightArrow from '../../assets/rightArrow.png'
+import {programsData} from "../../data/programsData"
+
 const Programs = () => {
   return (
     <div className="programs" id='programs'>
@@ -7,6 +10,19 @@ const Programs = () => {
             <span className='stroke-text'>Explore Our</span>
             <span>Programs</span>
             <span className='stroke-text'>to shape</span>
+        </div>
+        <div className="program-categories">
+           {programsData.map((program)=>(
+            <div className="category">
+              {program.image}
+              <span>{program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now">
+                <span>Join Now</span>
+                <span><img src={RightArrow} alt="" /></span>
+              </div>
+            </div>
+           ))} 
         </div>
 
     </div>
