c2c58beeb24000e9698a763810450c234536d6af
uday kumar <udaykumar@inr.group>
2022-08-10 12:42:33 +0530

    Reason Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
+import Reason from './Components/Reasons/Reason';
 function App() {
   return (
     <div className="App">
           <Hero/>
           <Programs/>
+          <Reason/>
     </div>
   );
 }
