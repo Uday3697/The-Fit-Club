04852d0135aca5f96bb9cb7fd22988ca1ba04fb9
uday kumar <udaykumar@inr.group>
2022-08-11 13:23:18 +0530

    Testinomials Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './Components/Hero/Hero';
 import Plans from './Components/Plans/Plans';
 import Programs from './Components/Programs/Programs';
 import Reason from './Components/Reasons/Reason';
+import Testimonials from './Components/Testimonials/Testimonials';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Programs/>
           <Reason/>
           <Plans/>
+          <Testimonials/>
     </div>
   );
 }
