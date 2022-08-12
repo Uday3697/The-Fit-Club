a1da6c3f0062e4e48d52032957b77e03580d7e92
uday kumar <udaykumar@inr.group>
2022-08-12 10:25:37 +0530

    footer section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ import Calories from "../../assets/calories.png";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="blur blur-h"> </div>
       <div className="left-h">
         <Header />
         <div className="the-best-ad">
